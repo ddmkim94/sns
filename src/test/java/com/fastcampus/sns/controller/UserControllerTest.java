7924152bfe0d74f1d10e770b5af08bfe40aaf158
ddmkim94 <ddmkim94@gmail.com>
2023-03-28 13:45:09 +0900

    #1 - UserController 테스트 작성
    UserController를 작성하고,
    로그인, 회원가입 성공, 실패를 검증하는 테스트 코드 작성함

---
+++ src/test/java/com/fastcampus/sns/controller/UserControllerTest.java
@@ -0,0 +1,107 @@
+package com.fastcampus.sns.controller;
+
+import com.fastcampus.sns.controller.request.UserJoinRequest;
+import com.fastcampus.sns.controller.request.UserLoginRequest;
+import com.fastcampus.sns.exception.SnsApplicationException;
+import com.fastcampus.sns.model.User;
+import com.fastcampus.sns.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void 회원가입() throws Exception {
+        String userName = "username";
+        String password = "password";
+
+        when(userService.join(userName, password)).thenReturn(mock(User.class));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 시 JSON 형태로 데이터 전달
+                        .content(objectMapper.writeValueAsString(new UserJoinRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 회원가입시_이미_회원가입된_username으로_회원가입을_하는경우_에러반환() throws Exception {
+        String userName = "username";
+        String password = "password";
+
+        when(userService.join(userName, password)).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new UserJoinRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void 로그인() throws Exception {
+        String userName = "username";
+        String password = "password";
+
+        when(userService.login(userName, password)).thenReturn("test_token");
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 시 JSON 형태로 데이터 전달
+                        .content(objectMapper.writeValueAsString(new UserLoginRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 로그인시_회원가입이_안된_userName을_입력할경우_에러반환() throws Exception {
+        String userName = "username";
+        String password = "password";
+
+        when(userService.login(userName, password)).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 시 JSON 형태로 데이터 전달
+                        .content(objectMapper.writeValueAsString(new UserLoginRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void 로그인시_틀린_password를_입력할경우_에러반환() throws Exception {
+        String userName = "username";
+        String password = "password";
+
+        when(userService.login(userName, password)).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 시 JSON 형태로 데이터 전달
+                        .content(objectMapper.writeValueAsString(new UserLoginRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+}
