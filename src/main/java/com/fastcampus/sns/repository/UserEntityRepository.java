86646fb3b5733839323b916131f7eb5a4bf1fd5f
ddmkim94 <ddmkim94@gmail.com>
2023-03-28 13:47:03 +0900

    #1 - userEntityRepository 추가

---
+++ src/main/java/com/fastcampus/sns/repository/UserEntityRepository.java
@@ -0,0 +1,11 @@
+package com.fastcampus.sns.repository;
+
+import com.fastcampus.sns.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// SimpleJpaRepository 에 @Repository 가 붙어있기 때문에 생략해도 상관없음
+public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUserName(String userName);
+}
