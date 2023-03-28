c8b47095b504a3b23993ea6f9b4476f99b21615d
ddmkim94 <ddmkim94@gmail.com>
2023-03-28 16:31:03 +0900

    #1 - 예외 클래스 구현
    ErrorCode를 반환하도록 구현

--- src/main/java/com/fastcampus/sns/exception/SnsApplicationException.java
+++ src/main/java/com/fastcampus/sns/exception/SnsApplicationException.java
@@ -1,5 +1,23 @@
 package com.fastcampus.sns.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 // TODO : 예외 클래스 추가 예정
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SnsApplicationException extends RuntimeException{
+
+    private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String getMessage() {
+        if (message == null) {
+            return errorCode.getMessage();
+        }
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
 }
