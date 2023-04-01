09ee508f632baaacc36a859f982612e95f2b3070
ddmkim94 <ddmkim94@gmail.com>
2023-04-01 16:46:30 +0900

    #5 - webSecurityCustomizer 메서드 추가
    api 관련 주소가 아닌 경우에는 시큐리티 필터가 동작하지 않도록 구현

--- src/main/java/com/fastcampus/sns/config/AuthenticationConfiguration.java
+++ src/main/java/com/fastcampus/sns/config/AuthenticationConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,12 +17,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class AuthenticationConfiguration{
+public class AuthenticationConfiguration {
 
     private final UserService userService;
     @Value("${jwt.secret-key}")
     private String secretKey;
-
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.csrf().disable()
@@ -38,5 +37,10 @@ public class AuthenticationConfiguration{
                 .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 .and()
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+        return (web) -> web.ignoring().regexMatchers("^(?!/api/).*");
     }
 }
